4f6c75d3ba2ea2553b03154f42036b36aa7d88be
野崎大生 <sauzerhokuto@gmail.com>
2022-09-07 16:27:04 +0900

    ダミーの current_userメソッドを定義し、記事作成のテスト実装を行う

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,14 +1,25 @@
 module Api::V1
-  class ArticlesController < ApplicationController
-    def index
-      articles = Article.all.order(updated_at: "DESC")
-      # レスポンスの値(id,title,updated_at)が複数の場合、 each_serializer を使用する。
-      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
-    end
+  class ArticlesController < BaseApiController
+    # def index
+    #   articles = Article.all.order(updated_at: "DESC")
+    #   # レスポンスの値(id,title,updated_at)が複数の場合、 each_serializer を使用する。
+    #   render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    # end
 
-    def show
-      article = Article.find(params[:id])
+    # def show
+    #   article = Article.find(params[:id])
+    #   render json: article, serializer: Api::V1::ArticlePreviewSerializer
+    # end
+
+    def create
+      article = current_user.articles.create!(article_params)
       render json: article, serializer: Api::V1::ArticlePreviewSerializer
     end
+
+    private
+
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
   end
 end
