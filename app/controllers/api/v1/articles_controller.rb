45567d0dd5051e6d0771e22d86ec14f9f55116f5
野崎大生 <sauzerhokuto@gmail.com>
2022-09-09 12:58:31 +0900

    記事削除のテスト実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,24 +1,30 @@
 module Api::V1
   class ArticlesController < BaseApiController
-    # def index
-    #   articles = Article.all.order(updated_at: "DESC")
-    #   # レスポンスの値(id,title,updated_at)が複数の場合、 each_serializer を使用する。
-    #   render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
-    # end
+    def index
+      articles = Article.all.order(updated_at: "DESC")
+      # レスポンスの値(id,title,updated_at)が複数の場合、 each_serializer を使用する。
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
 
-    # def show
-    #   article = Article.find(params[:id])
-    #   render json: article, serializer: Api::V1::ArticlePreviewSerializer
-    # end
+    def show
+      article = Article.find(params[:id])
+      render json: article, serializer: Api::V1::ArticlePreviewSerializer
+    end
 
-    # def create
-    #   article = current_user.articles.create!(article_params)
-    #   render json: article, serializer: Api::V1::ArticlePreviewSerializer
-    # end
+    def create
+      article = current_user.articles.create!(article_params)
+      render json: article, serializer: Api::V1::ArticlePreviewSerializer
+    end
 
     def update
       article = current_user.articles.find(params[:id])
       article.update!(article_params)
+      render json: article, serializer: Api::V1::ArticlePreviewSerializer
+    end
+
+    def destroy
+      article = current_user.articles.find(params[:id])
+      article.destroy!
       render json: article, serializer: Api::V1::ArticlePreviewSerializer
     end
 
--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -5,12 +5,11 @@ RSpec.describe "Articles", type: :request do
   #   subject { get(api_v1_articles_path) }
 
   #   context "記事のレコードが発行された場合" do
-  #     binding.pry
+
   #     let!(:article1) { create(:article, updated_at: 1.days.ago) }
   #     let!(:article){create(:article)}
   #     let!(:article2) { create(:article, updated_at: 2.days.ago) }
-  #     it "記事一覧を取得できる" do
-  #       binding.pry
+  #     fit "記事一覧を取得できる" do
   #       subject
   #       res = JSON.parse(response.body)
   #       a = Article.all.order(updated_at: "DESC")
@@ -22,67 +21,66 @@ RSpec.describe "Articles", type: :request do
   #   end
   # end
 
-  # describe "GET /api/v1/articles/:id" do
-  #   subject { get(api_v1_article_path(article_id)) }
+  describe "GET /api/v1/articles/:id" do
+    subject { get(api_v1_article_path(article_id)) }
 
-  #   context "指定したID(記事)に接続した時" do
-  #     let(:article) { create(:article) }
-  #     let(:article_id) { article.id }
-  #     it "指定のレコードの取得ができる" do
-  #       subject
-  #       res = JSON.parse(response.body)
-  #       # binding.pry
-  #       # レスポンスが200であること
-  #       expect(response).to have_http_status(:ok)
-  #       # 更新日が返ってくること、指定のレコードが返ってくること
-  #       expect(res["id"]).to eq article.id
-  #       expect(res["title"]).to eq article.title
-  #       expect(res["body"]).to eq article.body
-  #       expect(res["updated_at"]).to be_present
-  #       expect(res["user"]["id"]).to eq article.user_id
-  #       expect(res["user"].keys).to eq ["id", "name", "email"]
-  #     end
-  #   end
+    context "指定したID(記事)に接続した時" do
+      let(:article) { create(:article) }
+      let(:article_id) { article.id }
+      it "指定のレコードの取得ができる" do
+        subject
+        res = JSON.parse(response.body)
+        # binding.pry
+        # レスポンスが200であること
+        expect(response).to have_http_status(:ok)
+        # 更新日が返ってくること、指定のレコードが返ってくること
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res["user"]["id"]).to eq article.user_id
+        expect(res["user"].keys).to eq ["id", "name", "email"]
+      end
+    end
 
-  #   context "存在しないID(記事)に接続した時" do
-  #     let(:article_id) { 9999 }
-  #     it "レコードの取得ができない" do
-  #       expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
-  #     end
-  #   end
-  # end
+    context "存在しないID(記事)に接続した時" do
+      let(:article_id) { 9999 }
+      it "レコードの取得ができない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 
-  # describe "POST /api/v1/articles" do
-  #   subject { post(api_v1_articles_path, params: params) }
+  describe "POST /api/v1/articles" do
+    subject { post(api_v1_articles_path, params: params) }
 
-  #   context "適切なパラメーターを送信した時" do
-  #     before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+    context "適切なパラメーターを送信した時" do
+      before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
 
-  #     let(:current_user) { create(:user) }
-  #     let(:params) { { article: attributes_for(:article) } }
-  #     it "レコードが発行される" do
-  #       expect { subject }.to change { Article.count }.by(1)
-  #       res = JSON.parse(response.body)
-  #       expect(response).to have_http_status(:ok)
-  #       expect(res["title"]).to eq params[:article][:title]
-  #       expect(res["body"]).to eq params[:article][:body]
-  #       expect(res["title"]).to eq params[:article][:title]
-  #       expect(res["user"].keys).to eq ["id", "name", "email"]
-  #       expect(res["user"]["id"]).to eq current_user.id
-  #     end
-  #   end
+      let(:current_user) { create(:user) }
+      let(:params) { { article: attributes_for(:article) } }
+      it "レコードが発行される" do
+        expect { subject }.to change { Article.count }.by(1)
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(res["title"]).to eq params[:article][:title]
+        expect(res["body"]).to eq params[:article][:body]
+        expect(res["title"]).to eq params[:article][:title]
+        expect(res["user"].keys).to eq ["id", "name", "email"]
+        expect(res["user"]["id"]).to eq current_user.id
+      end
+    end
 
-  #   context "不適切なパラメータを送信した時" do
-  #     before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+    context "不適切なパラメータを送信した時" do
+      before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
 
-  #     let(:current_user) { create(:user) }
-  #     let(:params) { attributes_for(:article) }
-  #     it "レコードが発行されない" do
-  #       binding.pry
-  #       expect { subject }.to raise_error(ActionController::ParameterMissing)
-  #     end
-  #   end
-  # end
+      let(:current_user) { create(:user) }
+      let(:params) { attributes_for(:article) }
+      it "レコードが発行されない" do
+        expect { subject }.to raise_error(ActionController::ParameterMissing)
+      end
+    end
+  end
 
   describe "PATCH(PUT) /api/v1/articles/:id" do
     subject { patch(api_v1_article_path(article_id), params: params) }
@@ -95,7 +93,6 @@ RSpec.describe "Articles", type: :request do
     context "適切なパラメータを送信した時" do
       let(:article_id) { article.id }
       let(:article) { create(:article, user_id: current_user.id) }
-
       it "指定した内容だけレコードが更新される" do
         expect { subject }.to change { Article.find(article_id).title }.from(article.title).to(params[:article][:title]) &
                               not_change { Article.find(article_id).body } &
@@ -114,8 +111,30 @@ RSpec.describe "Articles", type: :request do
     end
   end
 
-  # describe "DELETE /api/v1/articles/:id" do
-  #   it "" do
-  #   end
-  # end
+  describe "DELETE /api/v1/articles/:id" do
+    subject { delete(api_v1_article_path(article_id)) }
+
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    let(:current_user) { create(:user) }
+    let(:article_id) { article.id }
+
+    context "対象のレコードを削除しようとした時" do
+      let!(:article) { create(:article, user: current_user) }
+
+      it "対象のレコードが削除される" do
+        expect { subject }.to change { Article.count }.by(-1)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "対象ではないレコードを削除しようとした時" do
+      let!(:article) { create(:article) }
+
+      it "レコードの削除ができない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound) &
+                              change { Article.count }.by(0)
+      end
+    end
+  end
 end
